daee9892fb421354a92418c4d7b8c8b87abdb54b
pauladkisson <paul.wesley.adkisson@gmail.com>
2025-02-21 17:40:31 -0800

    fixed MultiRecordingDataChunkIterator

--- src/jadhav_lab_to_nwb/olson_2024/olson_2024_convert_session.py
+++ src/jadhav_lab_to_nwb/olson_2024/olson_2024_convert_session.py
@@ -45,7 +45,7 @@ def session_to_nwb(
     # Get epoch info
     epoch_folder_paths = list(session_folder_path.glob(rf"{session_folder_path.name}_S[0-9][0-9]_F[0-9][0-9]_*"))
     epoch_folder_paths = sorted(epoch_folder_paths)
-    epoch_folder_paths = epoch_folder_paths[:1]  # TODO: Add the rest of the epochs when stub_test is False
+    # epoch_folder_paths = epoch_folder_paths[:1]  # TODO: Add the rest of the epochs when stub_test is False
 
     source_data = dict()
     conversion_options = dict()
@@ -86,7 +86,7 @@ def session_to_nwb(
     # Add DLC
     dlc_folder_path = session_folder_path / f"{session_folder_path.name}.DLC"
     file_paths = [file_path for file_path in dlc_folder_path.glob(r"*.csv") if not (file_path.name.startswith("._"))]
-    file_paths = file_paths[:1]  # TODO: Add the rest of the DLC files when stub_test is False
+    # file_paths = file_paths[:1]  # TODO: Add the rest of the DLC files when stub_test is False
     source_data.update(
         dict(
             DeepLabCut=dict(
@@ -128,7 +128,7 @@ if __name__ == "__main__":
     # Parameters for conversion
     data_dir_path = Path("/Volumes/T7/CatalystNeuro/Jadhav/SubLearnProject")
     output_dir_path = Path("/Volumes/T7/CatalystNeuro/Spyglass/raw")
-    stub_test = True
+    stub_test = False
 
     # Example Session
     subject_id = "SL18"
--- src/jadhav_lab_to_nwb/olson_2024/tools/spikeinterface.py
+++ src/jadhav_lab_to_nwb/olson_2024/tools/spikeinterface.py
@@ -2045,10 +2045,15 @@ class MultiRecordingDataChunkIterator(GenericDataChunkIterator):
         progress_bar_class: Optional[tqdm] = None,
         progress_bar_options: Optional[dict] = None,
     ):
-        self.dcis = []
+        self._dcis, self._start_frames, self._end_frames = [], [], []
+        num_frames = 0
         for recording in recordings:
             dci = SpikeInterfaceRecordingDataChunkIterator(recording=recording)
-            self.dcis.append(dci)
+            self._dcis.append(dci)
+            self._start_frames.append(num_frames)
+            num_frames += dci._get_maxshape()[0]
+            self._end_frames.append(num_frames)
+
         super().__init__(
             buffer_gb=buffer_gb,
             buffer_shape=buffer_shape,
@@ -2060,15 +2065,59 @@ class MultiRecordingDataChunkIterator(GenericDataChunkIterator):
         )
 
     def _get_default_chunk_shape(self, chunk_mb: float = 10.0) -> tuple[int, int]:
-        return self.dcis[0]._get_default_chunk_shape(chunk_mb=chunk_mb)
+        return self._dcis[0]._get_default_chunk_shape(chunk_mb=chunk_mb)
 
     def _get_data(self, selection: tuple[slice]) -> np.ndarray:
-        return np.concatenate([dci._get_data(selection=selection) for dci in self.dcis], axis=0)
+        start = selection[0].start
+        start = start if start is not None else 0
+        stop = selection[0].stop
+        stop = stop if stop is not None else self._get_maxshape()[0]
+        iterators_range = np.searchsorted(self._end_frames, (start, stop - 1), side="right")
+        iterators_spanned = list(range(iterators_range[0], min(iterators_range[-1] + 1, len(self._dcis))))
+
+        # If only one iterator is spanned, we can just return the data from that iterator
+        if len(iterators_spanned) == 1:
+            relative_start = start - self._start_frames[iterators_spanned[0]]
+            relative_stop = stop - self._start_frames[iterators_spanned[0]]
+            selection = (slice(relative_start, relative_stop), selection[1])
+            return self._dcis[iterators_spanned[0]]._get_data(selection=selection)
+
+        # If multiple iterators are spanned, we need to concatenate the data from each iterator
+        channel_start = selection[1].start
+        channel_start = channel_start if channel_start is not None else 0
+        channel_stop = selection[1].stop
+        channel_stop = channel_stop if channel_stop is not None else self._dcis[0]._get_maxshape()[1]
+        data = np.empty(shape=(stop - start, channel_stop - channel_start), dtype=self._get_dtype())
+        current_frame = 0
+
+        # Left endpoint (first iterator)
+        relative_start = start - self._start_frames[iterators_spanned[0]]
+        num_frames = self._end_frames[iterators_spanned[0]] - start
+        selection = (slice(relative_start, None), selection[1])
+        frame_slice = slice(current_frame, current_frame + num_frames)
+        data[frame_slice, :] = self._dcis[iterators_spanned[0]]._get_data(selection=selection)
+        current_frame += num_frames
+
+        # Inner iterators
+        selection = (slice(None, None), selection[1])
+        for i in iterators_spanned[1:-1]:
+            num_frames = self._end_frames[i] - self._start_frames[i]
+            frame_slice = slice(current_frame, current_frame + num_frames)
+            data[frame_slice, :] = self._dcis[i]._get_data(selection=selection)
+            current_frame += num_frames
+
+        # Right endpoint (last iterator)
+        relative_stop = num_frames = stop - self._start_frames[iterators_spanned[-1]]
+        selection = (slice(None, relative_stop), selection[1])
+        frame_slice = slice(current_frame, current_frame + num_frames)
+        data[frame_slice, :] = self._dcis[iterators_spanned[-1]]._get_data(selection=selection)
+
+        return data
 
     def _get_dtype(self):
-        return self.dcis[0]._get_dtype()
+        return self._dcis[0]._get_dtype()
 
     def _get_maxshape(self):
-        num_samples = sum([dci._get_maxshape()[0] for dci in self.dcis])
-        num_channels = self.dcis[0]._get_maxshape()[1]
+        num_samples = sum([dci._get_maxshape()[0] for dci in self._dcis])
+        num_channels = self._dcis[0]._get_maxshape()[1]
         return (num_samples, num_channels)
