57db56a3cda8b32e69a25867057e8c3589c482a0
pauladkisson <paul.wesley.adkisson@gmail.com>
2025-04-28 12:29:30 -0700

    updated nwbconverter

--- src/jadhav_lab_to_nwb/rivera_and_shukla_2025/rivera_and_shukla_2025_nwbconverter.py
+++ src/jadhav_lab_to_nwb/rivera_and_shukla_2025/rivera_and_shukla_2025_nwbconverter.py
@@ -1,14 +1,10 @@
 """Primary NWBConverter class for this dataset."""
 from neuroconv import NWBConverter
+from neuroconv.datainterfaces import DeepLabCutInterface
 
 from jadhav_lab_to_nwb.rivera_and_shukla_2025 import (
-    Olson2024BehaviorInterface,
-    Olson2024VideoInterface,
-    Olson2024DeepLabCutInterface,
-    Olson2024SpikeGadgetsRecordingInterface,
-    Olson2024SortingInterface,
-    Olson2024SpikeGadgetsLFPInterface,
-    Olson2024EpochInterface,
+    RiveraAndShukla2025BehaviorInterface,
+    RiveraAndShukla2025VideoInterface,
 )
 
 
@@ -16,11 +12,7 @@ class RiveraAndShukla2025NWBConverter(NWBConverter):
     """Primary conversion class."""
 
     data_interface_classes = dict(
-        Recording=Olson2024SpikeGadgetsRecordingInterface,
-        Sorting=Olson2024SortingInterface,
-        LFP=Olson2024SpikeGadgetsLFPInterface,
-        Video=Olson2024VideoInterface,
-        DeepLabCut=Olson2024DeepLabCutInterface,
-        Behavior=Olson2024BehaviorInterface,
-        Epoch=Olson2024EpochInterface,
+        Video=RiveraAndShukla2025VideoInterface,
+        Behavior=RiveraAndShukla2025BehaviorInterface,
+        DeepLabCut=DeepLabCutInterface,
     )
