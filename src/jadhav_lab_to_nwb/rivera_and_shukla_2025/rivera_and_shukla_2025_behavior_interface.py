ffcf4dc929800983068c15b3fa5a79addc6f50f0
pauladkisson <paul.wesley.adkisson@gmail.com>
2025-05-19 11:31:50 -0700

    removed unused imports

--- src/jadhav_lab_to_nwb/rivera_and_shukla_2025/rivera_and_shukla_2025_behavior_interface.py
+++ src/jadhav_lab_to_nwb/rivera_and_shukla_2025/rivera_and_shukla_2025_behavior_interface.py
@@ -2,11 +2,9 @@
 from pynwb.file import NWBFile
 from pydantic import FilePath
 import numpy as np
-
 from neuroconv.basedatainterface import BaseDataInterface
 from neuroconv.tools import nwb_helpers
 from neuroconv.utils import get_base_schema
-
 from pynwb.behavior import BehavioralEvents
 from pynwb import TimeSeries
 
