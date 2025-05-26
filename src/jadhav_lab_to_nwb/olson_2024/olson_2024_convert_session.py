b5792a0d409d9662c853386ec30a41c7b0b483ad
pauladkisson <paul.wesley.adkisson@gmail.com>
2025-05-26 13:07:30 -0700

    truncate epochs only for ephys when stubbing

--- src/jadhav_lab_to_nwb/olson_2024/olson_2024_convert_session.py
+++ src/jadhav_lab_to_nwb/olson_2024/olson_2024_convert_session.py
@@ -48,8 +48,6 @@ def session_to_nwb(
     # Get epoch info
     epoch_folder_paths = list(session_folder_path.glob(rf"{session_folder_path.name}_S[0-9][0-9]_F[0-9][0-9]_*"))
     epoch_folder_paths = sorted(epoch_folder_paths)
-    # if stub_test:  # TODO: Remove after this issue gets fixed: https://github.com/LorenFrankLab/spyglass/issues/1240
-    #     epoch_folder_paths = epoch_folder_paths[:1]
 
     source_data = dict()
     conversion_options = dict()
@@ -93,8 +91,6 @@ def session_to_nwb(
     # Add DLC
     dlc_folder_path = session_folder_path / f"{session_folder_path.name}.DLC"
     file_paths = [file_path for file_path in dlc_folder_path.glob(r"*.csv") if not (file_path.name.startswith("._"))]
-    # if stub_test:  # TODO: Remove after this issue gets fixed: https://github.com/LorenFrankLab/spyglass/issues/1240
-    #     file_paths = file_paths[:1]
     source_data.update(
         dict(
             DeepLabCut=dict(
